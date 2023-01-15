572cf4d6559b8df81162482fe8be0bbea5becf43
Brooklin <brooklinjazzmyers@gmail.com>
2023-01-15 15:47:44 -0800

    fix rectangle link not being passed navigate attr

--- lib/elixir_newbie_web/live/home_live.ex
+++ lib/elixir_newbie_web/live/home_live.ex
@@ -9,8 +9,7 @@ defmodule ElixirNewbieWeb.HomeLive do
     ~H"""
     <section class="flex h-fit w-full flex-col gap-12 4k:gap-36 lg:flex-row xl:gap-24">
       <article class="grid h-fit w-full grid-cols-6 grid-rows-6">
-        <article class="aspect-square relative col-start-1 col-end-6 row-start-1 row-end-6 rounded-full">
-          <div class="absolute h-full w-full rounded-full border-t-4 border-l-4 border-white"/>
+        <article class="aspect-square relative col-start-1 col-end-6 row-start-1 row-end-6 rounded-full border-t-4 border-l-4 border-white">
           <img
             class="rounded-full"
             alt="home page icon of a wizard"
@@ -19,34 +18,42 @@ defmodule ElixirNewbieWeb.HomeLive do
         </article>
         <.link
           href="https://twitter.com/BrooklinJMyers"
-          class="row-start-3 col-start-5 relative left-1/2 bottom-1/3 group"
+          class="row-start-3 col-start-5 relative left-1/2 bottom-1/3 group animate-fade-in"
         >
-          <div class="animate-fade-in absolute h-full w-full rounded-full border-4 border-dotted transition duration-300 ease-in-out group-hover:scale-110"/>
-          <img alt="Twitter Icon Button" class="rounded-full p-1" src="images/twitter_icon_reduced.webp"/>
+          <div class="transition duration-300 ease-in-out group-hover:scale-110">
+            <div class="animate-spin-slow absolute h-full w-full rounded-full border-l-2 border-cyan-600"/>
+            <img alt="Twitter Icon Button" class="rounded-full border-2" src="images/twitter_icon_reduced.webp"/>
+          </div>
           <p class="absolute -bottom-6 w-full text-center text-sm text-white">Twitter</p>
         </.link>
         <.link
           href="https://github.com/BrooklinJazz"
-          class="relative col-start-5 relative left-1/3 sm:left-1/4 row-start-4 group"
+          class="relative col-start-5 relative left-1/3 sm:left-1/4 row-start-4 group animate-fade-in"
         >
-          <div class="animate-fade-in absolute h-full w-full rounded-full border-4 border-dotted transition duration-300 ease-in-out group-hover:scale-110"/>
-          <img alt="GitHub Icon Button" class="rounded-full p-1" src="images/github_icon_reduced.webp"/>
+          <div class="transition duration-300 ease-in-out group-hover:scale-110">
+            <div class="animate-spin-slow absolute h-full w-full rounded-full border-l-2 border-green-600"/>
+            <img alt="GitHub Icon Button" class="rounded-full border-2" src="images/github_icon_reduced.webp"/>
+          </div>
           <p class="absolute -bottom-6 w-full text-center text-sm text-white">GitHub</p>
         </.link>
         <.link
           href="https://www.linkedin.com/in/brooklinmyers/"
-          class="row-start-5 col-start-4 relative left-1/2 group"
+          class="row-start-5 col-start-4 relative left-1/2 group animate-fade-in"
         >
-          <div class="animate-fade-in absolute h-full w-full rounded-full border-4 border-dotted transition duration-300 ease-in-out group-hover:scale-110"/>
-          <img alt="Linkedin Icon Button" class="rounded-full p-1" src="images/linkedin_icon_reduced.webp"/>
+          <div class="transition duration-300 ease-in-out group-hover:scale-110">
+            <div class="animate-spin-slow absolute h-full w-full rounded-full border-l-2 border-blue-600"/>
+            <img alt="Linkedin Icon Button" class="rounded-full border-2" src="images/linkedin_icon_reduced.webp"/>
+          </div>
           <p class="absolute -bottom-6 w-full text-center text-sm text-white">Linkedin</p>
         </.link>
         <.link
           href="https://github.com/DockYard-Academy/beta_curriculum"
-          class="row-start-5 col-start-3 relative left-1/4 top-1/2 group"
+          class="row-start-5 col-start-3 relative left-1/4 top-1/2 group animate-fade-in"
         >
-          <div class="animate-fade-in absolute h-full w-full rounded-full border-4 border-dotted transition duration-300 ease-in-out group-hover:scale-110"/>
-          <img alt="DockYard Academy Icon Button" class="rounded-full p-1" src="images/dockyard_academy_icon_reduced.webp"/>
+          <div class="transition duration-300 ease-in-out group-hover:scale-110">
+            <div class="animate-spin-slow absolute h-full w-full rounded-full border-l-2 border-black"/>
+            <img alt="DockYard Academy Icon Button" class="rounded-full border-2" src="images/dockyard_academy_icon_reduced.webp"/>
+          </div>
           <p class="absolute -bottom-12 w-full text-center text-sm text-white">DockYard Academy</p>
         </.link>
       </article>
@@ -63,6 +70,7 @@ defmodule ElixirNewbieWeb.HomeLive do
   def rectangle_link(assigns) do
     ~H"""
     <.link
+      navigate={@navigate}
       class={[
         "mt-4 xl:mt-8 text-left relative rounded-3xl aspect-[5/1]",
         "transition duration-500 ease-in-out ring ring-white hover:ring-offset-2"
