063d8a472b5aa67a410e26c0b962a84c6557e1d1
Bill Boz <Billboz1@gmail.com>
2023-02-21 22:26:48 -0500

    added livebook badge

--- lib/elixir_newbie_web/live/content_live.ex
+++ lib/elixir_newbie_web/live/content_live.ex
@@ -6,6 +6,7 @@ defmodule ElixirNewbieWeb.ContentLive do
     <section class="bg-[url('/images/background-smoke-transparent.webp')] min-h-screen bg-black bg-no-repeat bg-contain text-white">
       <.navigation />
       <section class="w-2/3 mx-auto">
+        <a class="bg-gray-500 hover:opacity-75 rounded-lg text-2xl p-6 mb-10 block" href={@badge_url}>Run in Livebook</a>
         <div class="markdown">
           <%= raw @lesson.body%>
         </div>
@@ -15,9 +16,10 @@ defmodule ElixirNewbieWeb.ContentLive do
   end
 
   def mount(%{"id" => id}, _session, socket) do
-    IO.inspect(socket.assigns.live_action)
     lesson = ElixirNewbie.AcademyContent.get_lesson(id, socket.assigns.live_action)
-
-    {:ok, assign(socket, lesson: lesson)}
+    url = "https://github.com/DockYard-Academy/curriculum/blob/main/#{socket.assigns.live_action}/#{id}.livemd"
+    query_params = URI.encode_query(%{"url" => url})
+    badge_url = "https://livebook.dev/run?" <> query_params
+    {:ok, assign(socket, lesson: lesson, badge_url: badge_url)}
   end
 end
